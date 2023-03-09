04e68aecace9cd237cdb47b928d869bde5a8302a
SerhiiShymko <shimko456@gmail.com>
2023-03-09 16:53:28 +0200

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+
+  console.log(bodyColor.style.backgroundColor);
+});
+
+btnStop.addEventListener('click', () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+
+  console.log(bodyColor.style.backgroundColor);
+});
